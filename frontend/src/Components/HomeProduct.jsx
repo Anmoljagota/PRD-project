8bb033ba8c77118cef59b0dc17d0485b2aa0552c
Anmoljagota <anmoljagota08@gmail.com>
2023-02-02 22:32:54 +0530

    product added to home page

--- frontend/src/Components/HomeProduct.jsx
+++ frontend/src/Components/HomeProduct.jsx
@@ -1,23 +1,51 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getdata } from '../Redux/Product_redux/action'
-import {Box, Image} from "@chakra-ui/react"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getdata } from "../Redux/Product_redux/action";
+import { Box, Image, SimpleGrid } from "@chakra-ui/react";
+import Styles from "../css/Project.module.css";
 const HomeProduct = () => {
-    const dispatch=useDispatch()
-    const data=useSelector((store)=>store.homeproduct)
-    console.log(data,"i am")
-    useEffect(()=>{
-      dispatch(getdata)
-    })
+  const dispatch = useDispatch();
+  const data = useSelector((store) => store.homeproduct);
+  console.log(data, "i am");
+  useEffect(() => {
+    dispatch(getdata);
+  }, []);
   return (
     <div>
-      {/* <Box border="1px solid blue">
-<Box>{data.length>0 && data.map((items)=>(
-  <Box><Image src={items.image}/></Box>
-))}</Box>
-      </Box> */}
+      <Box border="1px solid blue">
+        <Box border="1px solid yellow">
+          <SimpleGrid columns={[2, null, 5]} border="1px solid green">
+            {data.data &&
+              data.data.length > 0 &&
+              data.data.map((items) => (
+                <Box
+                  border="1px solid black"
+                  key={items._id}
+                  className={Styles.product}
+                >
+                  <Box
+                    height="200px"
+                    width="200px"
+                    position="relative"
+                    margin="0 auto"
+                  >
+                    <Image src={items.image} />
+                  </Box>
+                  <Box>{items.title}</Box>
+                  <Box
+                    color="#388e3c"
+                    paddingTop="8px"
+                    fontSize="16px"
+                  >{`Up to ${items.discount} off`}</Box>
+                  <Box display="inline-block"  mr="5px" textDecorationLine="line-through">{items.value}</Box>
+                  <Box display="inline-block" >{items.price}</Box>
+                </Box>
+              ))}
+          </SimpleGrid>
+        </Box>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default HomeProduct
+export default HomeProduct;
--- frontend/src/Redux/Product_redux/reducer.js
+++ frontend/src/Redux/Product_redux/reducer.js
@@ -3,15 +3,14 @@ const inital_state={
     loading:false,
     error:false,
     
-}
-const inital_state1={
     data:[]
 }
+
 const reducer=(state=inital_state,action)=>{
     const {type,payload}=action
     switch(type){
         case REQUEST:return {...state,loading:true,error:false}
-        case SUCCESS:return {...state,loading:false,error:false,data:{...inital_state1,data:payload}}
+        case SUCCESS:return {...state,loading:false,error:false,data:payload}
         case ERROR:return {...state,loading:false,error:true}
         default :return state
     }
